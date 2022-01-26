e9c06d5bdbd674920d8b5775bc20bec94467974e
clebson moura <clebson.sh@gmail.com>
2022-01-26 15:15:32 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
